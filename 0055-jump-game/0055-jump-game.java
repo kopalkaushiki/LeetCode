d384319729f6e60470e235579a447a1db2c3b2ca
Kopal Kaushiki <142652498+kopalkaushiki@users.noreply.github.com>
2026-09-25 18:58:11 +0530

    Time: 2 ms (89.62%), Space: 47.9 MB (42.39%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+       int final_pos= nums.length-1;
+        for(int jumps=nums.length-1; jumps>=0; jumps--){
+            if(jumps + nums[jumps] >=final_pos ){
+                final_pos=jumps;
+            } 
+        }
+        return final_pos ==0;
+    }
+}
